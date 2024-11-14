116a6d5302c6d9f0343b91686016a6fe0e77f986
karla <karlaarencio@hotmail.com>
2024-11-13 23:59:48 -0300

    FormModi.

--- InventarioTiendaElectronica/Form1.cs
+++ InventarioTiendaElectronica/Form1.cs
@@ -16,5 +16,10 @@ namespace InventarioTiendaElectronica
         {
             InitializeComponent();
         }
-    }
+
+		private void label5_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
