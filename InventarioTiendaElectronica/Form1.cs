4a13af97aa4803b953abb6209a0ca8ad7f19fdb9
Fiorella Rizzo <Rizzofiorella@outlook.com>
2024-11-14 01:25:36 -0300

    Capa Negocio completa con sus clases

--- InventarioTiendaElectronica/Form1.cs
+++ InventarioTiendaElectronica/Form1.cs
@@ -26,11 +26,16 @@ namespace InventarioTiendaElectronica
         {
 
         }
+
+        private void textCategoria_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 
-		private void label5_Click(object sender, EventArgs e)
-		{
-
-		}
-	}
 }
